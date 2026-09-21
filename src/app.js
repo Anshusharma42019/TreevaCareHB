aaad4feec232fded1545fea45d3bbe9828e86736
Anshu Sharma <Anshusharma42019@gmail.com>
2026-09-21 13:37:53 +0530

    feat: initialize Express application with middleware, routing, and error handling

--- src/app.js
+++ src/app.js
@@ -28,6 +28,9 @@ const allowedOrigins = [
   'http://localhost:5000',
   'http://127.0.0.1:5500',
   'http://localhost:5500',
+  "https://treev.trivenayurveda.in",
+  "https://treeva.in"
+
 ];
 app.use(
   cors({
